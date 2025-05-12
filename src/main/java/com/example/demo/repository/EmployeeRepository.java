33b4643a3893db4b4a9a339ef8ab9d24d2cf0803
PJ-JP <a20213805@pucp.edu.pe>
2025-05-11 20:39:36 -0500

    feat: pregunta 5

--- src/main/java/com/example/demo/repository/EmployeeRepository.java
+++ src/main/java/com/example/demo/repository/EmployeeRepository.java
@@ -2,6 +2,7 @@ package com.example.demo.repository;
 
 
 
+import com.example.demo.dto.ReportDTO;
 import com.example.demo.entity.Employee;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
@@ -12,37 +13,21 @@ import java.util.List;
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
-  /*  @Query("SELECT e FROM Employee e " +
-            "WHERE LOWER(e.firstName) LIKE %:search% " +
-            "OR LOWER(e.lastName) LIKE %:search% " +
-            "OR LOWER(e.job.jobTitle) LIKE %:search% " +
-            "OR LOWER(e.department.departmentName) LIKE %:search% " +
-            "OR LOWER(e.department.location.city) LIKE %:search%")
-    List<Employee> findBySearch(@Param("search") String search);
+    @Query("SELECT e FROM Employee e " +
+            "WHERE CONCAT(LOWER(e.firstName),' ', LOWER(e.lastName)) LIKE %:search%")
+    List<Employee> findByName(@Param("search") String search);
 
-    // Para reporte de empleados con salario > 15000
-    @Query("SELECT e FROM Employee e WHERE e.salary > :amount")
-    List<Employee> findEmployeesWithSalaryGreaterThan(@Param("amount") double amount);
+    @Query(value = "select concat(employees.first_name,' ',employees.last_name) as empleado,jobs.min_salary,jobs.max_salary,jobs.job_id,nombre,employees.salary,promedio from employees, jobs\n" +
+            "inner join (select concat(first_name,' ',last_name) as nombre,job_id as job_id_new,promedio from employees \n" +
+            "inner join (select job_id as new_job_id, max(salary) as max_salary, avg(salary) as promedio from employees group by job_id) subQuery \n" +
+            "on ((employees.salary=subQuery.max_salary) and employees.job_id=subQuery.new_job_id)) subQ\n" +
+            "on((jobs.job_id=subQ.job_id_new)) where employees.job_id=jobs.job_id;",nativeQuery = true)
+    List<ReportDTO> findReport();
 
-    // Para reporte de gerentes con experiencia > 5 años
-    @Query(value = """
-        SELECT DISTINCT e.* FROM employees e
-        JOIN job_history jh ON e.employee_id = jh.employee_id
-        WHERE DATEDIFF(CURDATE(), jh.start_date) > 5 * 365
-        AND e.employee_id IN (SELECT manager_id FROM departments)
-        """, nativeQuery = true)
-    List<Employee> findExperiencedManagers();
-
-    @Query(value = """
-    SELECT e.first_name as firstName, e.last_name as lastName,
-           jh.start_date as startDate, jh.end_date as endDate,
-           j.job_title as jobTitle
-    FROM employees e
-    JOIN job_history jh ON e.employee_id = jh.employee_id
-    JOIN jobs j ON jh.job_id = j.job_id
-    WHERE e.salary > :salary
-    """, nativeQuery = true)
-    List<JobReportProjection> findWorkHistoryOfEmployeesWithHighSalary(@Param("salary") double salary);
-
-*/
+    @Query(value = "select concat(employees.first_name,' ',employees.last_name) as empleado,jobs.min_salary,jobs.max_salary,jobs.job_id,nombre,employees.salary,promedio from employees, jobs\n" +
+            "inner join (select concat(first_name,' ',last_name) as nombre,job_id as job_id_new,promedio from employees \n" +
+            "inner join (select job_id as new_job_id, max(salary) as max_salary, avg(salary) as promedio from employees group by job_id) subQuery \n" +
+            "on ((employees.salary=subQuery.max_salary) and employees.job_id=subQuery.new_job_id)) subQ\n" +
+            "on((jobs.job_id=subQ.job_id_new)) where employees.job_id=jobs.job_id and lower(concat(employees.first_name,' ',employees.last_name)) like %?1%;",nativeQuery = true)
+    List<ReportDTO> findReportSearch(@Param("search") String search);
 }
